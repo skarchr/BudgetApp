9f7c3b17930ed6885cd816d73ce6bb7a591b0f2e
Christian Skar <skarchr@gmail.com>
2015-04-03 20:10:47 +0200

    First commit

---
+++ BudgetApp/Startup.cs
@@ -0,0 +1,15 @@
+﻿using BudgetApp;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace BudgetApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
