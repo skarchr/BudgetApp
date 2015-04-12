f0a5b5a8253c2865f7d1bd63bf9200dfb834cd18
Christian Skar <skarchr@gmail.com>
2015-04-12 23:11:05 +0200

    Added Transaction model and started with an enum for categories

---
+++ BudgetApp/Models/Transaction.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BudgetApp.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        public double Amount { get; set; }
+
+        public Category Category { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    public enum Category
+    {
+        #region Food
+
+        Groceries,
+        Restaurant,
+        Treats,
+        OtherFood,
+
+        #endregion
+
+        #region Shelter
+
+        Furniture,
+        Interior,
+        Rent,
+        Mortgage,
+        Utilities,
+        RenovationRepairsMaintenance,
+        OtherShelter,
+
+        #endregion
+
+        #region Transportation
+
+        Car,
+        Fuel,
+        Repairs,
+        Maintenance,
+        Parts,
+        Plane,
+        CollectiveTransport,
+        OtherTransportation,
+
+        #endregion
+
+        #region Personal
+        
+        Phone,
+        Hobby,
+        Vacation,
+        Clothes,
+        Entertainment,
+        Subscriptions,
+        OtherPersonal,
+
+        #endregion
+
+        #region Fixed
+        
+        Insurance,
+        Medical,
+        Dental,
+        DebtReduction,
+        Other
+
+        #endregion
+
+
+    }
+}
