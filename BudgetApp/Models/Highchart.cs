05d2cd9ca3546bf5c83b402cfd366f15be058709
Christian Skar <skarchr@gmail.com>
2015-04-19 11:40:22 +0200

    Added DataLabels to highchart model

--- BudgetApp/Models/Highchart.cs
+++ BudgetApp/Models/Highchart.cs
@@ -12,11 +12,17 @@ namespace BudgetApp.Models
 
     public class Data
     {
+        public Data()
+        {
+            DataLabels = new DataLabels();
+        }
+
         public double Y { get; set; }
         public double X { get; set; }
         public string Color { get; set; }
         public string Drilldown { get; set; }
         public string Name { get; set; }
+        public DataLabels DataLabels { get; set; }
     }
 
     public class Drilldown
@@ -43,5 +49,16 @@ namespace BudgetApp.Models
         public string FontSize { get; set; }
         public string FontWeight { get; set; }
         public string TextShadow { get; set; }
+    }
+
+    public class DataLabels
+    {
+        public DataLabels()
+        {
+            Enabled = true;
+            Format = "{point.y:,.1f}";
+        }
+        public bool Enabled { get; set; }
+        public string Format { get; set; }
     }
 }
