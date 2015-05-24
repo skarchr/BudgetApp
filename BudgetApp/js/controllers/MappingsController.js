11a29baa23b34558eebd532c34b5eb6fd9fcdf6e
Christian Skar <skarchr@gmail.com>
2015-05-24 13:14:28 +0200

    Changes to Mapping view

--- BudgetApp/js/controllers/MappingsController.js
+++ BudgetApp/js/controllers/MappingsController.js
@@ -6,6 +6,8 @@
         $scope.model = mappingModel.model;
         $scope.urls = mappingModel.urls;
 
+        $scope.filteredCategory = '';
+
         $scope.getIcon = function(input) {
 
             return commonService.getIconUrl(input);
@@ -14,19 +16,26 @@
 
         $scope.categories = mappingModel.categories;
 
+        $scope.addMapping = function(category) {
+            alert('Helloooo');
+        };
+
+        $scope.setCategory = function(category) {
+            $scope.filteredCategory = category;
+        };
+
         $scope.countCategory = function(category) {
 
             var count = 0;
 
             var cat1 = category.replace(/([A-Z])/g, " $1");
 
-            var cat2  = (cat1.charAt(0).toUpperCase() + cat1.slice(1)).toString();
+            var cat2  = (cat1.charAt(0).toUpperCase() + cat1.slice(1)).trim();
 
             for (var i = 0; i < mappingModel.model.length; i++) {
-                //TODO: Fix counter
-                if (mappingModel.model[i].category.toString() == cat2) {
+
+                if (mappingModel.model[i].category.trim() === cat2) {
                     count += 1;
-                    console.log('++');
                 }
             }
             return count;
