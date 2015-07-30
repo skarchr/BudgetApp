f8762f3b920f9eb7eb626e84f65ca68089abf937
Christian Skar <skarchr@gmail.com>
2015-07-30 21:25:55 +0200

    Added pagination to transactions

--- BudgetApp/js/filters/Filters.js
+++ BudgetApp/js/filters/Filters.js
@@ -38,6 +38,13 @@
 
     angular.module('budgetApp')
 
+        .filter('startFrom', function () {
+            return function (input, start) {
+                start = +start; //parse to int
+                return input.slice(start);
+            }
+        })
+
         .filter('currencyFormatter', function () {
 
             return function (input, input2) {
