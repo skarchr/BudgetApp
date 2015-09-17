12a591c38ee921fa3c7b3c0a66562768585aab59
Christian Skar <skarchr@gmail.com>
2015-09-17 22:59:00 +0200

    Added an admin page

---
+++ BudgetApp/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BudgetApp.Models;
+
+namespace BudgetApp.Controllers
+{
+    [Authorize(Roles = "Administrators")]
+    public class AdminController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Admin
+        public ActionResult Index()
+        {
+
+            var users =  db.Users.ToList();
+
+            return View(users);
+        }
+
+    }
+}
