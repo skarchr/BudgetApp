3fd2e0b90d251e79b9e7ac1258ef9b3016484243
Christian Skar <skarchr@gmail.com>
2015-12-04 00:11:40 +0100

    Added configuration of excel file uploader

--- BudgetApp/Controllers/TransactionsController.cs
+++ BudgetApp/Controllers/TransactionsController.cs
@@ -220,12 +220,17 @@ namespace BudgetApp.Controllers
         {
             ViewBag.Error = TempData["Error"];
 
-            return View();
+            return View(new ExcelColumns());
         }
 
         [HttpPost]
-        public ActionResult Upload(HttpPostedFileBase file)
+        public ActionResult Upload(ExcelColumns excelColumns)
         {
+            if(excelColumns == null)
+                excelColumns = new ExcelColumns();
+
+            var file = excelColumns.File;
+
             if (file != null && file.ContentLength > 0)
             {
                 var fileName = Path.GetFileName(file.FileName);
@@ -243,7 +248,7 @@ namespace BudgetApp.Controllers
 
                 int found;
 
-                var transactions = ExcelReader.ReadFile(path, User.Identity.Name, out found);
+                var transactions = ExcelReader.ReadFile(path, User.Identity.Name, excelColumns, out found);
 
                 ViewBag.Info = string.Format("Found {0} new transactions", found);
 
---
+++ BudgetApp/Models/ExcelColumns.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BudgetApp.Models
+{
+    public class ExcelColumns
+    {
+        public ExcelColumns()
+        {
+            DateCol = 0;
+            DescriptionCol = 1;
+            AmountCols = new List<int> { 2,3,4 };
+            DateFormat = "dd.MM.yyyy";
+        }
+
+        public HttpPostedFileBase File { get; set; }
+
+        public string DateFormat { get; set; }
+        public int DateCol { get; set; }
+        public int DescriptionCol { get; set; }
+        public List<int> AmountCols { get; set; }
+    }
+}
